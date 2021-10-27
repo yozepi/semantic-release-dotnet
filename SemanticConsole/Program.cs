748f1a1f636ff37e0a3ba1f3f7680fac209bd062
Joe Cleland <joe.cleland@eventcore.com>
2021-10-26 22:48:59 -0700

    chore: Initial solution setup

---
+++ SemanticConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace SemanticConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assem = Assembly.GetExecutingAssembly();
+            string assemblyVersion = assem.GetName().Version.ToString();
+            string fileVersion = FileVersionInfo.GetVersionInfo(assem.Location).FileVersion;
+
+            Console.WriteLine("Semantic version example app!");
+            Console.WriteLine("Assembly version:\t{0}", assemblyVersion);
+            Console.WriteLine("File version:\t\t{0}", fileVersion);
+        }
+    }
+}
