78e2c6b8bc67a952e19fcc5a5f87d2bc5a516619
Joe Cleland <joe.cleland@eventcore.com>
2021-11-01 16:02:17 -0700

    fix: Replace double-line with single-line

--- SemanticConsole/Program.cs
+++ SemanticConsole/Program.cs
@@ -13,7 +13,7 @@ namespace SemanticConsole
             string fileVersion = FileVersionInfo.GetVersionInfo(assem.Location).FileVersion;
 
             Console.WriteLine("Semantic version example app!");
-            Console.WriteLine("=================================");
+            Console.WriteLine("---------------------------------");
             Console.WriteLine("Assembly version:\t{0}", assemblyVersion);
             Console.WriteLine("File version:\t\t{0}", fileVersion);
         }
